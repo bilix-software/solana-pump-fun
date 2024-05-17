3feb40a7b6e940f12e0fd448a1736f861da5fe81
Bilix <info@bilix.io>
2024-05-17 13:38:31 +0200

    removed compiled files for clarity

--- src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionMode = void 0;
-var TransactionMode;
-(function (TransactionMode) {
-    TransactionMode[TransactionMode["Simulation"] = 0] = "Simulation";
-    TransactionMode[TransactionMode["Execution"] = 1] = "Execution";
-})(TransactionMode || (exports.TransactionMode = TransactionMode = {}));
