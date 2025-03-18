3e31476b12fdac68f9dbb8f9768aa6f6a6486889
dp <dp0x7ce@gmail.com>
2025-03-18 12:33:13 +0800

    Update api.ts
    pump.fun has updated their API endpoint.

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getCoinData(mintStr: string) {
     try {
-        const url = `https://frontend-api.pump.fun/coins/${mintStr}`;
+        const url = `https://frontend-api-v3.pump.fun/coins/${mintStr}`;
         const response = await axios.get(url, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
